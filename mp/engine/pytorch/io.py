31811ef0faaef22aa0b16ac6f73d437ba7c1943a
kerryeon <besqer996@gnu.ac.kr>
2018-11-16 19:07:52 +0900

    Travis installation test (Try: 1)

--- mp/engine/pytorch/io.py
+++ mp/engine/pytorch/io.py
@@ -1,4 +1,5 @@
 from mp.engine.python.io import IO as _IO
+from mp.engine.python.io import _attr as _attr_python
 from mp.engine.pytorch import attribute as _attr
 from mp.engine.pytorch.device import Device
 
@@ -13,5 +14,9 @@ class IO(_IO):
 
     @classmethod
     def _save_binary(cls, path: str, value):
-        raw_np = value.cpu().numpy()
+        # is PyTorch Tensor
+        if type(value) is _attr.torch.Tensor:
+            raw_np = value.cpu().numpy()
+        else:
+            raw_np = _attr_python.np.array(value)
         super()._save_binary(path, raw_np)
